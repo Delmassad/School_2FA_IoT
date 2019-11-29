efe4d7731270379fb0e4c3ce2c32d3f460b3a510
alex <alexandre.a.vigny@gmail.com>
2019-11-29 10:54:27 +0100

    works now
    yay

--- python/FaceDetection/__init__.py
+++ python/FaceDetection/__init__.py
@@ -2,19 +2,19 @@
 import time
 from os import listdir, makedirs
 from os.path import isfile, join, isdir
-from miscelaneous import listFiles, listDir
+from miscelaneous import listFiles, listDir, convertToGray
 
 import cv2
 import matplotlib.pyplot as plt
-
-from miscelaneous import convertToGray
 
 # Global variables for the classifier's detection
 SCALE_FACTOR = 1.1
 MIN_NEIGHBORS = 6
 
-#load cascade classifier training file for haarcascade
-haar_face_cascade = cv2.CascadeClassifier('./FaceDetection/haarcascade_frontalface_alt.xml')
+# load cascade classifier training file for haarcascade
+haar_face_cascade = cv2.CascadeClassifier(
+    './FaceDetection/haarcascade_frontalface_alt.xml')
+
 
 def getFacesHaar(RGB_Img, classifier=haar_face_cascade):
     """
@@ -22,12 +22,14 @@ def getFacesHaar(RGB_Img, classifier=haar_face_cascade):
     """
     gray_img = convertToGray(RGB_Img)
     print(len(gray_img))
-    #let's detect multiscale (some images may be closer to camera than others) images
-    faces = classifier.detectMultiScale(gray_img, scaleFactor=SCALE_FACTOR, minNeighbors=MIN_NEIGHBORS)
+    # let's detect multiscale (some images may be closer to camera than others) images
+    faces = classifier.detectMultiScale(
+        gray_img, scaleFactor=SCALE_FACTOR, minNeighbors=MIN_NEIGHBORS)
     faces_img = []
     for (x, y, w, h) in faces:
         faces_img.append(RGB_Img[y:y+h, x:x+w])
     return faces_img
+
 
 def dataTransfer(sourcePath, username, destPath):
     rawPhotosPaths = listFiles(sourcePath)
